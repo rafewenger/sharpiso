559b5bc619f95a4f45b4bb1256fc375d763b53f5
Rephael Wenger <wenger@cse.ohio-state.edu>
2012-03-04 09:48:58 -0500

    Reindented/reformatted files. Added anisograd_print_coord.

--- src/anisogradinfo/anisogradinfo.cxx
+++ src/anisogradinfo/anisogradinfo.cxx
@@ -1,11 +1,17 @@
-#include "anisogradinfo.h"
 
-#include "anisograd.h"
-#include "anisograd_operators.h"
+#include <iostream>
 #include <iomanip>
 
+#include "anisogradinfo.h"
+#include "anisograd.h"
+#include "anisograd_operators.h"
 
 using namespace std;
+
+// **************************************************
+// COMPUTE
+// **************************************************
+
 void compute_curvature 
 (const SHARPISO_SCALAR_GRID_BASE & scalar_grid,
  const GRADIENT_GRID & gradient_grid,
@@ -14,114 +20,120 @@ void compute_curvature
  const VERTEX_INDEX iv1,
  ANISOINFO_TYPE  &aniso_info)
 {
-    int icube = iv1;
-    aniso_info.iv1 = iv1;
-    // Compute M d for direction 'd' for  vertex iv1
-    compute_m_d
-    ( scalar_grid, gradient_grid, icube, iv1, 0, aniso_info.mX);
+  int icube = iv1;
+  aniso_info.iv1 = iv1;
+
+  // Compute M d for direction 'd' for  vertex iv1
+  compute_m_d( scalar_grid, gradient_grid, icube, iv1, 0, aniso_info.mX);
+  compute_m_d( scalar_grid, gradient_grid, icube, iv1, 1, aniso_info.mY);
+  compute_m_d( scalar_grid, gradient_grid, icube, iv1, 2, aniso_info.mZ);
     
-    compute_m_d
-    ( scalar_grid, gradient_grid, icube, iv1, 1, aniso_info.mY);
-    
-    compute_m_d
-    ( scalar_grid, gradient_grid, icube, iv1, 2, aniso_info.mZ);
-    
-    // compute the curvature k for a vertex iv1
-    compute_curvature_iv 
+  // compute the curvature k for a vertex iv1
+  compute_curvature_iv 
     (scalar_grid, gradient_grid, iv1, aniso_info.K);
     
-    for (int d=0; d<DIM3; d++)
-        compute_g_x(mu, aniso_info.K[d],aniso_info.flag_aniso, aniso_info.gK[d]);
+  for (int d=0; d<DIM3; d++)
+    compute_g_x(mu, aniso_info.K[d],aniso_info.flag_aniso, aniso_info.gK[d]);
     
-    compute_gradH_d_scalar_grid(scalar_grid, iv1, aniso_info.dirc, aniso_info.gradientS_d);
+  compute_gradH_d_scalar_grid
+    (scalar_grid, iv1, aniso_info.dirc, aniso_info.gradientS_d);
     
-    compute_c_d
+  compute_c_d
     ( scalar_grid, gradient_grid, iv1, aniso_info.dirc, aniso_info.c);
     
-    
-    compute_forward_difference_d_normals
+  compute_forward_difference_d_normals
     ( gradient_grid, iv1, aniso_info.dirc, aniso_info.fwd_diff_d_normals);
     
-    compute_forward_difference_d
+  compute_forward_difference_d
     (scalar_grid, iv1, aniso_info.dirc, aniso_info.fwd_diff_d);
     
-    // Compute operator gradH for the direction 'd'
-    // for the Normal field
-    compute_gradH_d_normals 
+  // Compute operator gradH for the direction 'd'
+  // for the Normal field
+  compute_gradH_d_normals 
     (gradient_grid,iv1, aniso_info.gradH_d_normals_direc, 
      aniso_info.gradientH_d_Normals);
     
 };
 
 
+// **************************************************
+// PRINT
+// **************************************************
 
-// info 
+template <typename CTYPE>
+void aniso_print_coord(std::ostream & out, const CTYPE coord[DIM3])
+{
+  const int OWIDTH = 10;
+
+  out << "(";
+  for (int i = 0; i < DIM3; i++) {
+    out << setw(10);
+    out << coord[i];
+    if (i+1 < DIM3) { out << ","; };
+  }
+  out << ")";
+}
+
+
 void print_info
 (
  const SHARPISO_SCALAR_GRID_BASE & scalar_grid,
  const GRADIENT_GRID & gradient_grid,
  const ANISOINFO_TYPE aniso_info)
 {
-    if (aniso_info.flag_k){
-    cout <<"K vertex( "<< aniso_info.iv1<<") iter "<<aniso_info.iter<<" is ";
+  if (aniso_info.flag_k){
+    cout <<"  K: ";
+    aniso_print_coord(cout, aniso_info.K);
+
+    cout <<" Gk: ";
+    aniso_print_coord(cout, aniso_info.gK);
+    cout << endl;
     
-    cout <<" ("<<setw(10)<<aniso_info.K[0]<<","<<setw(10)<<aniso_info.K[1]<<","<<setw(10)<<aniso_info.K[2]<<")";
-    
-    cout <<" Gk ";
-    
+    /* OBSOLETE
     cout <<" ("<<setw(10)<<aniso_info.gK[0]<<","<<setw(10)<<aniso_info.gK[1]<<","<<setw(10)<<aniso_info.gK[2]<<")"<<endl;
-    }
-    if (aniso_info.flag_m) {
-        cout <<" m "<<endl;
-        cout <<"x("<<aniso_info.mX[0]<<" "<<aniso_info.mX[1]<<" "<<aniso_info.mX[2]<<")"<<endl;
-        cout <<"y("<<aniso_info.mY[0]<<" "<<aniso_info.mY[1]<<" "<<aniso_info.mY[2]<<")"<<endl;
-        cout <<"z("<<aniso_info.mZ[0]<<" "<<aniso_info.mZ[1]<<" "<<aniso_info.mZ[2]<<")"<<endl;
+    */
+  }
+  if (aniso_info.flag_m) {
+    cout <<" m "<<endl;
+    cout <<"x("<<aniso_info.mX[0]<<" "<<aniso_info.mX[1]<<" "<<aniso_info.mX[2]<<")"<<endl;
+    cout <<"y("<<aniso_info.mY[0]<<" "<<aniso_info.mY[1]<<" "<<aniso_info.mY[2]<<")"<<endl;
+    cout <<"z("<<aniso_info.mZ[0]<<" "<<aniso_info.mZ[1]<<" "<<aniso_info.mZ[2]<<")"<<endl;
         
-    }
+  }
     
-    if(aniso_info.print_c)
-    {
-        
-//        cout <<"gradientS_d in the direction "<<aniso_info.dirc<<" is ";
-//        cout <<"("<<aniso_info.gradientS_d[0]<<" "<<aniso_info.gradientS_d[1]<<" "<<aniso_info.gradientS_d[2]<<")"<<endl;
-        cout <<"c in the direction "<<aniso_info.dirc <<" is ";
-        cout <<"("<<aniso_info.c[0]<<","<<aniso_info.c[1]<<","<<aniso_info.c[2]<<")"<<endl;
-//        cout <<"N fwd diff in direction "<<aniso_info.dirc<<" is ";
-//        cout <<"("<<aniso_info.fwd_diff_d_normals[0]<<" "<<aniso_info.fwd_diff_d_normals[1]<<" "<<
-//        aniso_info.fwd_diff_d_normals[2]<<")"<<endl;
-//        cout <<"fwd diff in direction "<<aniso_info.dirc<<" is ";
-//        cout <<"("<<aniso_info.fwd_diff_d<<")"<<endl;
+  if(aniso_info.print_c) {
+      cout <<"c in the direction "<<aniso_info.dirc <<" is ";
+      cout <<"("<<aniso_info.c[0]<<","<<aniso_info.c[1]<<","<<aniso_info.c[2]<<")"<<endl;
+  }
+
+  if (aniso_info.print_gradientH_d_normals) {
+
+    cout<<"gradientH_d normals in the direction "
+        <<aniso_info.gradH_d_normals_direc<<" is (";
+    for (int k=0; k<DIM9; k++) {
+      if(k%3==0)
+        cout <<"\n";
+      cout<<" "<<aniso_info.gradientH_d_Normals[k];
     }
-    if (aniso_info.print_gradientH_d_normals)
-    {
-        cout<<"gradientH_d normals in the direction "<<aniso_info.gradH_d_normals_direc<<" is (";
-        for (int k=0; k<DIM9; k++) {
-            if(k%3==0)
-                cout <<"\n";
-            cout<<" "<<aniso_info.gradientH_d_Normals[k];
-        }
-        cout <<")\n";
-    }
+    cout <<")\n";
+  }
     
-    if(aniso_info.flag_normals){
+  if(aniso_info.flag_normals){
     cout <<"("<<aniso_info.normals[0]<<","<<aniso_info.normals[1]<<","<<aniso_info.normals[2]<<")"<<endl;
         
-        for (int i=0; i<DIM3; i++) {
-           VERTEX_INDEX prev = scalar_grid.PrevVertex(aniso_info.iv1, i);
-           VERTEX_INDEX next = scalar_grid.NextVertex(aniso_info.iv1, i);
-            const GRADIENT_COORD_TYPE *gr_prev;
-            const GRADIENT_COORD_TYPE *gr_next;
-            gr_prev=gradient_grid.VectorPtrConst(prev);
-            cout <<"prev vert in direction "<<i<<" ";
-            cout <<"("<<gr_prev[0]<<","<<gr_prev[1]<<","<<gr_prev[2]<<")"<<endl;
-            gr_next=gradient_grid.VectorPtrConst(next);
-            cout <<"next vert in direction "<<i<<" ";
-            cout <<"("<<gr_next[0]<<","<<gr_next[1]<<","<<gr_next[2]<<")"<<endl;
-
-            
-        }
-        
-    
+    for (int i=0; i<DIM3; i++) {
+      VERTEX_INDEX prev = scalar_grid.PrevVertex(aniso_info.iv1, i);
+      VERTEX_INDEX next = scalar_grid.NextVertex(aniso_info.iv1, i);
+      const GRADIENT_COORD_TYPE *gr_prev;
+      const GRADIENT_COORD_TYPE *gr_next;
+      gr_prev=gradient_grid.VectorPtrConst(prev);
+      cout <<"prev vert in direction "<<i<<" ";
+      cout <<"("<<gr_prev[0]<<","<<gr_prev[1]<<","<<gr_prev[2]<<")"<<endl;
+      gr_next=gradient_grid.VectorPtrConst(next);
+      cout <<"next vert in direction "<<i<<" ";
+      cout <<"("<<gr_next[0]<<","<<gr_next[1]<<","<<gr_next[2]<<")"<<endl;
     }
+    
+  }
     
 }
--- src/anisogradinfo/anisogradinfo_main.cxx
+++ src/anisogradinfo/anisogradinfo_main.cxx
@@ -3,23 +3,23 @@
 /// Version 0.1.0
 
 /*
- IJK: Isosurface Jeneration Kode
- Copyright (C) 2011 Rephael Wenger
+  IJK: Isosurface Jeneration Kode
+  Copyright (C) 2011 Rephael Wenger
  
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public License
- (LGPL) as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  (LGPL) as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
  
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
  
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 
 #include <iostream>
@@ -75,110 +75,103 @@ ANISOINFO_TYPE aniso_info;
 
 int main(int argc, char **argv)
 {
-    time_t start_time;
-    time(&start_time);
+  time_t start_time;
+  time(&start_time);
     
-    vector<GRADIENT_COORD_TYPE> mag_list;
+  vector<GRADIENT_COORD_TYPE> mag_list;
     
-    IJK::ERROR error;
+  IJK::ERROR error;
     
-    try {
+  try {
         
-        std::set_new_handler(memory_exhaustion);
+    std::set_new_handler(memory_exhaustion);
         
-        parse_command_line(argc, argv);
+    parse_command_line(argc, argv);
         
-        SHARPISO_SCALAR_GRID full_scalar_grid;
-        GRID_NRRD_IN<int,int> nrrd_in;
-        NRRD_DATA<int,int> nrrd_header;
+    SHARPISO_SCALAR_GRID full_scalar_grid;
+    GRID_NRRD_IN<int,int> nrrd_in;
+    NRRD_DATA<int,int> nrrd_header;
      
         
-        nrrd_in.ReadScalarGrid
-        (scalar_filename, full_scalar_grid, nrrd_header, error);
+    nrrd_in.ReadScalarGrid
+      (scalar_filename, full_scalar_grid, nrrd_header, error);
         
-        if (nrrd_in.ReadFailed()) { throw error; }
+    if (nrrd_in.ReadFailed()) { throw error; }
         
-        GRADIENT_GRID gradient_grid;
+    GRADIENT_GRID gradient_grid;
         
-        if (flag_cdiff) 
-        {
-            compute_gradient_central_difference
-            (full_scalar_grid, icube, gradient_grid);
-        }
-        else
-        {
-            // compute the central gradients first 
-            compute_gradient_central_difference(full_scalar_grid, icube, gradient_grid);
-            
-            normalize_and_store_gradient_magnitudes(full_scalar_grid, gradient_grid, mag_list);
-            
-            if (flag_iso) 
-            {
-                // Calculate the anisotropic diff of the gradients.
-                const int dimension = full_scalar_grid.Dimension();
-                gradient_grid.SetSize(full_scalar_grid, dimension);
-                for (int k=0; k<num_iter; k++) 
-                {
-                    aniso_info.iter=k;
-                    if(flag_icube){
-                        aniso_info.flag_aniso = 0;
-                        compute_curvature(full_scalar_grid, gradient_grid, mu, lambda, icube, aniso_info);
-                        print_info(full_scalar_grid, gradient_grid, aniso_info);
-                    }
-                    
-                    anisotropic_diff_iter_k
-                    (full_scalar_grid, mu, lambda, k, 0, icube, dimension, gradient_grid);
-                }            
-            }
-            else
-            {
-                cout << "Anisostropic gradients called."<<endl;
-                const int dimension = full_scalar_grid.Dimension();
-                gradient_grid.SetSize(full_scalar_grid, dimension);
-                for (int k=0; k<num_iter; k++) 
-                {
-                    aniso_info.iter=k;
-                    cout <<"iteration "<<k<<endl;
-                    if(flag_icube)
-                    {
-                        aniso_info.flag_aniso = 1;
-                        aniso_info.normals = 
-                        gradient_grid.VectorPtr(icube);
-                        compute_curvature
-                        (full_scalar_grid, gradient_grid, mu, lambda, icube, 
-                         aniso_info);
-                        print_info(full_scalar_grid, gradient_grid,aniso_info);
-                    }
-                    anisotropic_diff_iter_k
-                    (full_scalar_grid, mu, lambda, k, 1, icube, dimension, gradient_grid);
-                }
-            }
-        }
-        
-        if (report_time_flag) {
-            
-            time_t end_time;
-            time(&end_time);
-            double total_elapsed_time = difftime(end_time, start_time);
-            
-            cout << endl;
-            cout << "Elapsed time = " << total_elapsed_time << " seconds." 
-            << endl;
-        };
-        
-    } 
-    catch (ERROR error) {
-        if (error.NumMessages() == 0) {
-            cerr << "Unknown error." << endl;
-        }
-        else { error.Print(cerr); }
-        cerr << "Exiting." << endl;
-        exit(20);
+    if (flag_cdiff) {
+      compute_gradient_central_difference
+        (full_scalar_grid, icube, gradient_grid);
     }
-    catch (...) {
-        cerr << "Unknown error." << endl;
-        exit(50);
+    else {
+      // compute the central gradients first 
+      compute_gradient_central_difference(full_scalar_grid, icube, gradient_grid);
+            
+      normalize_and_store_gradient_magnitudes(full_scalar_grid, gradient_grid, mag_list);
+            
+      if (flag_iso) {
+
+        // Calculate the anisotropic diff of the gradients.
+        const int dimension = full_scalar_grid.Dimension();
+        gradient_grid.SetSize(full_scalar_grid, dimension);
+        for (int k=0; k<num_iter; k++) {
+          aniso_info.iter=k;
+          if(flag_icube) {
+            aniso_info.flag_aniso = 0;
+            compute_curvature
+              (full_scalar_grid, gradient_grid, mu, lambda, icube, aniso_info);
+            print_info(full_scalar_grid, gradient_grid, aniso_info);
+          }
+                    
+          anisotropic_diff_iter_k
+            (full_scalar_grid, mu, lambda, k, 0, icube, dimension, gradient_grid);
+        }            
+      }
+      else {
+        cout << "Anisostropic gradients called."<<endl;
+        const int dimension = full_scalar_grid.Dimension();
+        gradient_grid.SetSize(full_scalar_grid, dimension);
+        for (int k=0; k<num_iter; k++) {
+          aniso_info.iter=k;
+          cout <<"iteration "<<k<<endl;
+          if(flag_icube) {
+            aniso_info.flag_aniso = 1;
+            aniso_info.normals = gradient_grid.VectorPtr(icube);
+            compute_curvature
+              (full_scalar_grid, gradient_grid, mu, lambda, icube, aniso_info);
+            print_info(full_scalar_grid, gradient_grid,aniso_info);
+          }
+          anisotropic_diff_iter_k
+            (full_scalar_grid, mu, lambda, k, 1, icube, dimension, gradient_grid);
+        }
+      }
+    }
+        
+    if (report_time_flag) {
+            
+      time_t end_time;
+      time(&end_time);
+      double total_elapsed_time = difftime(end_time, start_time);
+            
+      cout << endl;
+      cout << "Elapsed time = " << total_elapsed_time << " seconds." 
+           << endl;
     };
+        
+  } 
+  catch (ERROR error) {
+    if (error.NumMessages() == 0) {
+      cerr << "Unknown error." << endl;
+    }
+    else { error.Print(cerr); }
+    cerr << "Exiting." << endl;
+    exit(20);
+  }
+  catch (...) {
+    cerr << "Unknown error." << endl;
+    exit(50);
+  };
     
 }
 
@@ -189,104 +182,99 @@ int main(int argc, char **argv)
 
 void memory_exhaustion()
 {
-    cerr << "Error: Out of memory.  Terminating program." << endl;
-    exit(10);
+  cerr << "Error: Out of memory.  Terminating program." << endl;
+  exit(10);
 }
 
 void parse_command_line(int argc, char **argv)
 {
-    int iarg = 1;
+  int iarg = 1;
     
-    while (iarg < argc && argv[iarg][0] == '-') {
-        if (string(argv[iarg]) == "-time") 
-        { report_time_flag = true;   }
-        else if (string(argv[iarg]) == "-gzip")
-        { flag_gzip = true; }
-        else if (string(argv[iarg]) == "-cdiff")
-        { flag_cdiff = true; }
-        else if (string(argv[iarg]) == "-iso")
-        { flag_iso = true; }
-        else if (string(argv[iarg]) == "-k")
-        { aniso_info.flag_k = true; }
-        else if (string(argv[iarg]) == "-m")
-        { aniso_info.flag_m = true; }
-        else if (string(argv[iarg]) == "-n")
-        { aniso_info.flag_normals = true; }
-        else if (string(argv[iarg]) == "-c")
-        {
-            aniso_info.print_c = true;
-            iarg++;
-            if (iarg >= argc) { usage_error(); };
-            sscanf(argv[iarg], "%d", &aniso_info.dirc);
-        }
-        else if (string(argv[iarg]) == "-gradH_d_Normals")
-        {
-            aniso_info.print_gradientH_d_normals= true;
-            iarg++;
-            if (iarg >= argc) { usage_error(); };
-            sscanf(argv[iarg], "%d", &aniso_info.gradH_d_normals_direc);
-        }
-        else if (string(argv[iarg]) == "-mu")
-        {
-            iarg++;
-            if (iarg >= argc) { usage_error(); };
-            sscanf(argv[iarg], "%f", &mu);
-        }
-        else if (string(argv[iarg]) == "-lambda")
-        {
-            iarg++;
-            if (iarg >= argc) { usage_error(); };
-            sscanf(argv[iarg], "%f", &lambda);
-        }
-        else if (string(argv[iarg]) == "-num_iter")
-        {
-            iarg++;
-            if (iarg >= argc) { usage_error(); };
-            sscanf(argv[iarg], "%d", &num_iter);
-        }
-        else if (string(argv[iarg]) == "-icube")
-        {
-            iarg++;
-            flag_icube = true;
-            if (iarg >= argc) { usage_error(); };
-            sscanf(argv[iarg], "%d", &icube);
-        }
-        else 
-        { usage_error(); }
+  while (iarg < argc && argv[iarg][0] == '-') {
+    if (string(argv[iarg]) == "-time") 
+      { report_time_flag = true;   }
+    else if (string(argv[iarg]) == "-gzip")
+      { flag_gzip = true; }
+    else if (string(argv[iarg]) == "-cdiff")
+      { flag_cdiff = true; }
+    else if (string(argv[iarg]) == "-iso")
+      { flag_iso = true; }
+    else if (string(argv[iarg]) == "-k")
+      { aniso_info.flag_k = true; }
+    else if (string(argv[iarg]) == "-m")
+      { aniso_info.flag_m = true; }
+    else if (string(argv[iarg]) == "-n")
+      { aniso_info.flag_normals = true; }
+    else if (string(argv[iarg]) == "-c")
+      {
+        aniso_info.print_c = true;
         iarg++;
-    }
+        if (iarg >= argc) { usage_error(); };
+        sscanf(argv[iarg], "%d", &aniso_info.dirc);
+      }
+    else if (string(argv[iarg]) == "-gradH_d_Normals")
+      {
+        aniso_info.print_gradientH_d_normals= true;
+        iarg++;
+        if (iarg >= argc) { usage_error(); };
+        sscanf(argv[iarg], "%d", &aniso_info.gradH_d_normals_direc);
+      }
+    else if (string(argv[iarg]) == "-mu")
+      {
+        iarg++;
+        if (iarg >= argc) { usage_error(); };
+        sscanf(argv[iarg], "%f", &mu);
+      }
+    else if (string(argv[iarg]) == "-lambda")
+      {
+        iarg++;
+        if (iarg >= argc) { usage_error(); };
+        sscanf(argv[iarg], "%f", &lambda);
+      }
+    else if (string(argv[iarg]) == "-num_iter")
+      {
+        iarg++;
+        if (iarg >= argc) { usage_error(); };
+        sscanf(argv[iarg], "%d", &num_iter);
+      }
+    else if (string(argv[iarg]) == "-icube")
+      {
+        iarg++;
+        flag_icube = true;
+        if (iarg >= argc) { usage_error(); };
+        sscanf(argv[iarg], "%d", &icube);
+      }
+    else 
+      { usage_error(); }
+    iarg++;
+  }
     
-    if (iarg+1 != argc) { usage_error(); };
+  if (iarg+1 != argc) { usage_error(); };
     
-    scalar_filename = argv[iarg];
+  scalar_filename = argv[iarg];
     
 }
 
 void usage_msg()
 {
-    cerr <<"Usage: anisogradinfo [options]  {scalar nrrd file}"<<endl;
-    cerr <<"                 [-gzip] [-time]"<<endl;
-    cerr <<"                 [-icube]    cube  index "<<endl; 
-    cerr <<"                 [-cdiff]    central difference"<<endl;
-    cerr <<"                 [-iso]      isotropic diffusion "<<endl;
-    cerr <<"                 [-mu]       extent of anisotropic diffusion"<< endl; 
-    cerr <<"                 [-lambda]   extent of diffusion in each iteration " <<endl; 
-    cerr <<"                 [-num_iter] number of iterations "<<endl;
-    cerr <<"                  -k prints the curvature"<<endl;
-    cerr <<"                  -n prints normals"<<endl;
-    cerr <<"                  -m prints the m  values for the vertex"<<endl;
-    cerr <<"                  -c <d>  d is the direction{0 1 2} prints the c for that direction "<<endl;
-    cerr <<"                  -gradH_d_Normals <d> d is the direction "<<endl;
-    cerr << endl;
+  cerr <<"Usage: anisogradinfo [options]  {scalar nrrd file}"<<endl;
+  cerr <<"                 [-gzip] [-time]"<<endl;
+  cerr <<"                 [-icube]    cube  index "<<endl; 
+  cerr <<"                 [-cdiff]    central difference"<<endl;
+  cerr <<"                 [-iso]      isotropic diffusion "<<endl;
+  cerr <<"                 [-mu]       extent of anisotropic diffusion"<< endl; 
+  cerr <<"                 [-lambda]   extent of diffusion in each iteration " <<endl; 
+  cerr <<"                 [-num_iter] number of iterations "<<endl;
+  cerr <<"                  -k prints the curvature"<<endl;
+  cerr <<"                  -n prints normals"<<endl;
+  cerr <<"                  -m prints the m  values for the vertex"<<endl;
+  cerr <<"                  -c <d>  d is the direction{0 1 2} prints the c for that direction "<<endl;
+  cerr <<"                  -gradH_d_Normals <d> d is the direction "<<endl;
+  cerr << endl;
 }
 
 void usage_error()
 {
-    usage_msg();
-    exit(100);
+  usage_msg();
+  exit(100);
 }
-
-
-
-
-
