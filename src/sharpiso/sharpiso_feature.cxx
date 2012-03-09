9cbc9e2362a166c01dd8f9111f554b8726d25ccd
Arindam Bhattacharya <bhattacharya.29@osu.edu>
2012-03-08 21:15:34 -0500

    local commit

--- src/sharpiso/sharpiso_feature.cxx
+++ src/sharpiso/sharpiso_feature.cxx
@@ -89,7 +89,6 @@ void SHARPISO::svd_compute_sharp_vertex_for_cube
 
     GRADIENT_COORD_TYPE ray_direction[DIM3]={0.0};
 
-    //debug
     /*
     svd_calculate_sharpiso_vertex
     (&(point_coord[0]), &(gradient_coord[0]), &(scalar[0]),
