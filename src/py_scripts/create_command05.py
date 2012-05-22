bf29f3f30ca41513de7db279da73ae5204af0eb8
arindam bhattacharya <arindamb86@gmail.com>
2012-05-22 00:37:21 -0400

    added the create_comamnd05.py which creates a list of isodual3D commands for the negative gradients

--- src/py_scripts/create_command05.py
+++ src/py_scripts/create_command05.py
@@ -1,10 +1,11 @@
+# create command05 this is to run the negate commands 
 import subprocess as sp
 
-def create_iso_command(args):
+def create_iso_command_negate(args):
   outlist=[]
   #read the data sheet info 
   fda=open ('testData/data_sheet.txt','r')
-  print 'create_iso_command'
+  print 'create_iso_command_negative gradients'
   for pos in args.position:
     output=[]
     print 'position ',pos
@@ -18,55 +19,64 @@ def create_iso_command(args):
         output.append(infile_temp[0])
         output.append(dir)
         output.append(iso)
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #repostion
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #lindstrom
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-lindstrom', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-lindstrom', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #allow_conflict
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-allow_conflict', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-allow_conflict', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #centroid_conflcit
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-centroid_conflict', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-centroid_conflict', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #clamp_far
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-clamp_far', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-clamp_far', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #reselectg
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reselectg', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reselectg', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #allow conflict + repos
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-allow_conflict', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-allow_conflict', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #clamp conflict + repos
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-clamp_conflict', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-clamp_conflict', iso,\
+                 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
@@ -78,7 +88,7 @@ def create_iso_command(args):
 
 
 ############### run with cgradient
-def create_iso_command_cgrad(args):
+def create_iso_command_negate_cgrad(args):
   outlist=[]
   #read the data sheet info 
   fda=open ('testData/data_sheet.txt','r')
@@ -96,62 +106,62 @@ def create_iso_command_cgrad(args):
         output.append(infile_temp[0])
         output.append(dir)
         output.append(iso)
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-gradient',\
-                 'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-gradient',\
+                  'testData/'+infile_temp[0]+'neg.cgrad.nrrd', iso, 'testData/'+infile_temp[0]+'neg.nrrd'])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #repostion
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-gradient',\
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-gradient',\
                  'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #lindstrom
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-lindstrom','-gradient',\
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-lindstrom','-gradient',\
                  'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #allow_conflict
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-allow_conflict','-gradient',\
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-allow_conflict','-gradient',\
                  'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #centroid_conflcit
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-centroid_conflict','-gradient',\
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-centroid_conflict','-gradient',\
                  'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #clamp_far
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-clamp_far','-gradient',\
+        sp.call(['./isodual3D','-trimesh','-multi_isov','-s','-position',pos,'-o', 'out.off','-clamp_far','-gradient',\
                  'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #reselectg
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reselectg','-gradient',\
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-reselectg','-gradient',\
                  'testData/'+infile_temp[0]+'.cgrad.nrrd', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #allow conflict + repos
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-allow_conflict', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-allow_conflict', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
         output.append(opsplit[1])
         #clamp conflict + repos
-        sp.call(['./isodual3D','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-clamp_conflict', iso, 'testData/'+infile])
+        sp.call(['./isodual3D','-multi_isov','-trimesh','-s','-position',pos,'-o', 'out.off','-reposition','-clamp_conflict', iso, 'testData/'+infile])
         sp.call(['./findedge','140','out.off'])
         op=sp.check_output(['./findEdgeCount','-fp','out.line'])
         opsplit=op.split()
--- src/py_scripts/test_script09.py
+++ src/py_scripts/test_script09.py
@@ -8,25 +8,30 @@ import datetime
 args=cl.parse_command_line()
 
 dt=str(dt.datetime.now())
-'''
 
+tab_width=6
+
+#debug 
+print 'original options', args.position
+args.position=['gradEC', 'gradNS']
 print '=====output==='
 op=cc.create_iso_command(args)
 for iv,out in enumerate(op):
   fname='out'+args.position[iv]+dt+'.txt'
   fi=open (fname,'w')
   print >>fi,args.position[iv]
-  print >>fi,df.tests
+  print >>fi,df.TEST_HEADER_STRING1
+  print >>fi,df.TEST_HEADER_STRING2
   for out_sub in out:
     if out_sub != 'break':
-      print >>fi,out_sub.ljust(5,' '),
+      print >>fi,out_sub.ljust(tab_width,' '),
     else:
       print >>fi,'\n',
   print 'output written to',fname,']' 
   
 
 
-      
+   
 ################################## cgradient
 op=cc.create_iso_command_cgrad(args)
 print '=====output cgradient==='
@@ -35,24 +40,50 @@ for iv,out in enumerate(op):
   fname='outc'+args.position[iv]+dt+'.txt'
   fi=open (fname,'w')
   print >>fi,args.position[iv]
-  print >>fi,df.tests
+  print >>fi,df.TEST_HEADER_STRING1
+  print >>fi,df.TEST_HEADER_STRING2
   for out_sub in out:
     if out_sub != 'break':
-      print >>fi,out_sub.ljust(5,' '),
+      print >>fi,out_sub.ljust(tab_width,' '),
     else:
       print >>fi,'\n',
   print 'output written to',fname,']' 
   
+
+################################## negative gradient
+import create_command05 as cc5
+op=cc5.create_iso_command_negate(args)
+print '=====output negative gradient==='
+args.position =  ['gradCD','gradIES','gradIEDir']
+args.isovalue =  ['-10.1','-10.6'] 
+
+for iv,out in enumerate(op):
+  fname='outneg'+args.position[iv]+dt+'.txt'
+  fi=open (fname,'w')
+  print >>fi,args.position[iv]
+  print >>fi,df.TEST_HEADER_STRING1
+  print >>fi,df.TEST_HEADER_STRING2
+  for out_sub in out:
+    if out_sub != 'break':
+      print >>fi,out_sub.ljust(tab_width,' '),
+    else:
+      print >>fi,'\n',
+  print 'output written to',fname,']' 
+  
+
+
+  
 '''
 ################################# neg 
+import create_command04 as cc
 print '=====output negative==='
 fname='outneg'+dt+'.txt'
 op=cc.create_iso_command_neg(args) 
 fi=open (fname,'w')
 for out_sub in op:
   if out_sub !='break':
-    print >>fi,out_sub.ljust(5,' '),
+    print >>fi,out_sub.ljust(tab_width,' '),
   else:
     print >>fi,'\n',
 print 'output written to',fname,']'
-  
+'''  
