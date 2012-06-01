f4e23251ae21b62a0f10c0c8912e59743d2f5966
Arindam Bhattacharya <arindamb86@gmail.com>
2012-06-01 13:04:14 -0400

    updated scripts

--- src/py_scripts/test_script10.py
+++ src/py_scripts/test_script10.py
@@ -10,6 +10,7 @@ OPTIONS
 #types = ['annulus', 'two_cubes']
 types = ['annulus']
 positions = ['gradCD', 'gradEC','gradNS','gradIES','gradIEDir','gradCS','centroid']
+#positions = ['gradCD', 'gradEC','gradNS']
 iso_cmd = "isodual3D"
 def_parms = ['-trimesh', '-multi_isov','-sep_pos', '-s', '-o','out.off']
 OPTS = []
@@ -34,6 +35,7 @@ function to set up the isovalues
 def set_isov(typ):
   if typ == "annulus":
     return ['10.1', '10.2','10.4','10.7']
+    #return ['10.1', '10.8']
   if typ == "two_cubes":
     return ['15.1', '15.2','15.5','15.8']  
   
@@ -83,7 +85,6 @@ def run_tests():
 '''
 print the results 
 '''
-'''
 def print_res(res):
   fi=open ('result.txt','w')
   print 'printing the results', len(res)
@@ -102,12 +103,89 @@ def print_res(res):
     for val in row[3]:
       print >>fi,val.ljust(7), 
     print >>fi,'\n',
-'''    
-#new print     
-def print_res(res):
+ 
+
+    
+def print_res2(res):
+    ranklist = [0]*len(OPTS)
+    fi = open ('results_summary.txt','w')
+    
+    for i in range(len(positions)):
+        ####################
+        #setup the counters for the opts 
+        clmpconflict_cnt=0
+        repo_cnt=0
+        allw_conflct_cnt=0
+        lind_cnt=0
+        cntrd_conflct=0
+        clamp_far_cnt=0
+        reselectg_cnt=0
+        all_conf_repo_cnt=0
+        min0=0
+        ####################
+        for row in res: 
+            pos_vals=row[3]
+            opts_name=(' ').join(row[2])
+            #print 'name',opts_name
+            vals = map(int,pos_vals)
+            minlist= min(vals)
+            maxlist= max(vals)
+            if minlist == vals[i] and minlist!=maxlist:
+                if (opts_name=='-clamp_conflict'):
+                    clmpconflict_cnt=clmpconflict_cnt+1
+                elif (opts_name=='-reposition'):
+                    repo_cnt=repo_cnt+1
+                elif (opts_name=='-allow_conflict'):
+                    allw_conflct_cnt = allw_conflct_cnt+1
+                elif (opts_name=='-lindstrom'):
+                    lind_cnt = lind_cnt+1
+                elif (opts_name=='-centroid_conflict'):
+                    cntrd_conflct=cntrd_conflct+1
+                elif (opts_name=='-clamp_far'):
+                    clamp_far_cnt=clamp_far_cnt+1
+                elif (opts_name=='-reselectg'):
+                    reselectg_cnt=reselectg_cnt+1
+                elif (opts_name=='-allow_conflict -reposition'):
+                    all_conf_repo_cnt=all_conf_repo_cnt+1
+            if minlist == 0:
+                min0=min0+1
+            
+        print >>fi,'for ',positions[i],' out of ',len(res),'tests'
+        print >>fi,'-clamp_conflict was at the top for'.ljust(40),clmpconflict_cnt
+        print >>fi,'-reposition was best for '.ljust(40),repo_cnt
+        print >>fi,'-lindstrom was best for'.ljust(40),lind_cnt
+        print >>fi,'-allow_conflict was best for'.ljust(40),allw_conflct_cnt
+        print >>fi,'-centroid_conflict was best for'.ljust(40),cntrd_conflct
+        print >>fi,'-clamp_far was best for '.ljust(40),clamp_far_cnt
+        print >>fi,'-reselectg was best for'.ljust(40),reselectg_cnt
+        print >>fi,'-allow_conflict -reposition was best for'.ljust(40),all_conf_repo_cnt
+        print >>fi,'the number of errors were 0 in times'.ljust(40),min0
+        print >>fi,'************************************************'
+            
+            
+            
+
+'''
+main function 
+'''
+def main ():
+  print 'welcome to isodual tests'
+  res = run_tests()
+  print_res(res)
+  print_res2(res)
+  
+
+if __name__ == "__main__":
+    main()
+    
+
+
+##OBSOLETE     
+def print_res3(res):
   fi=open ('result.txt','w')
   print 'printing the results', len(res)
   for row in res:
+    print row
     vals=row[3]
     vals = [map(int, x) for x in vals]
     names_of_vals=positions[:]
@@ -118,20 +196,6 @@ def print_res(res):
     for y in sortednames:
         print >>fi,"%7s"% y,    
     print >>fi,''    
-  
-
-'''
-main function 
-'''
-def main ():
-  print 'welcome to isodual tests'
-  res = run_tests()
-  print_res(res)
-  
-
-if __name__ == "__main__":
-    main()
-    
 
       
   
--- src/py_scripts/test_script11.py
+++ src/py_scripts/test_script11.py
@@ -4,9 +4,9 @@ import subprocess as sp
 global run_tests  
 global set_isov
 global print_res
+OPTS = []
 # set up the names 
-opt_name=['clmp_cnflct','repo','lind','allow_cnflct','cntrd_cnflct','clmp_far','rslctg'\
-  ,'all_cnlf&repo']
+opts_name=[]
 
 '''
 OPTIONS
@@ -15,22 +15,31 @@ OPTIONS
 #types = ['annulus', 'two_cubes']
 types = ['annulus']
 positions = ['gradCD', 'gradEC','gradNS','gradIES','gradIEDir','gradCS','centroid']
-
+#positions = ['gradCD', 'gradEC','gradNS']
 iso_cmd = "isodual3D"
 def_parms = ['-trimesh', '-multi_isov','-sep_pos', '-s', '-o', 'positions.off']
 '''
 set up the isodual3D commend
 '''
 def setup_isocmd():
-  OPTS = []
   OPTS.append(['-clamp_conflict'])
+  opts_name.append('clmConf')
   OPTS.append(['-reposition'])
+  opts_name.append('repo')
   OPTS.append(['-lindstrom'])
+  opts_name.append('lind')
   OPTS.append(['-allow_conflict'])
+  opts_name.append('alwCnf')
   OPTS.append(['-centroid_conflict'])
+  opts_name.append('cntrdCnf')
   OPTS.append(['-clamp_far'])
+  opts_name.append('clpFr')
   OPTS.append(['-reselectg'])
+  opts_name.append('rslctG')
   OPTS.append(['-allow_conflict','-reposition'])
+  opts_name.append('alwCf&repo')
+  OPTS.append(['-centroid_conflict','-reposition']) 
+  opts_name.append('cntrdCnf&repo')
   return OPTS
 
 
@@ -40,6 +49,7 @@ function to set up the isovalues
 def set_isov(typ):
   if typ == "annulus":
     return ['10.1', '10.2','10.4','10.7']
+    #return ['10.1', '10.7']
   if typ == "two_cubes":
     return ['15.1', '15.2','15.5','15.8']  
   
@@ -79,7 +89,34 @@ def run_tests():
   return row_lists 
   
 
-
+def print_res2(res):
+    fi=open ('results_large.txt','w')
+    for i in range (len(positions)):
+        print >>fi,'Test on ',positions[i], 'num of tests ',len(res)
+        print >>fi,'filename iso',
+        for ele in range(len(OPTS)):
+            print >>fi,opts_name[ele].ljust(8),
+        print >>fi,''
+        cnt_list=[0]*len(OPTS)
+        for row in res:
+            if str(row[2])==positions[i]: 
+                print >>fi,row[0].split('.')[0].ljust(7),
+                print >>fi,row[1].ljust(3),
+                for ele in row[3]:
+                    print >>fi,ele.ljust(8),          
+                vals=row[3]
+                vals=map(int,vals)
+                minlist = min(vals)
+                maxlist = max(vals)
+                print >>fi,'| ',minlist,maxlist
+                for ind,n in enumerate(vals):
+                    if n==minlist:
+                       cnt_list[ind]=cnt_list[ind]+1
+        #print 'cnt_list ',cnt_list
+        print >>fi,'\n \n SUMMARY'
+        for ele in range(len(cnt_list)):
+            print >>fi,'%15s was best in %5s cases'% (OPTS[ele], cnt_list[ele])        
+        print >>fi,'*************************************\n'           
 
 '''
 print the results 
@@ -89,6 +126,8 @@ def print_res(res):
   print 'printing the results', len(res)
 
   for row in res:
+    print row 
+    '''
     vals=row[3]
     vals = [map(int, x) for x in vals]
     names_of_vals=opt_name[:]
@@ -96,7 +135,7 @@ def print_res(res):
     x.sort()
     sortedvals,sortednames=zip(*x)  
     print >>fi,row[0], row[1], row[2], sortednames
-
+    '''
 
 '''
 main function 
@@ -104,7 +143,7 @@ main function
 def main ():
   print 'welcome to isodual tests'
   res = run_tests()
-  print_res(res)
+  print_res2(res)
   
 
 if __name__ == "__main__":
