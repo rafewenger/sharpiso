d48213fedee353ef5555c7e63a1362e3e84240e8
Arindam Bhattacharya <arindamb86@gmail.com>
2012-06-22 15:25:20 -0400

    updated findEdgeCount

--- src/findEdgeCount/findEdgeCountIO.cxx
+++ src/findEdgeCount/findEdgeCountIO.cxx
@@ -16,116 +16,123 @@ int cntMoreThan3(0);
 
 void output_edges
 (const int dim, 
- const COORD_TYPE * coord,
- const int numv,
- const VERTEX_INDEX * edge_vert,
- const int nume,
- vector<int> vert_degree)
+		const COORD_TYPE * coord,
+		const int numv,
+		const VERTEX_INDEX * edge_vert,
+		const int nume,
+		vector<int> vert_degree)
 {
-  for (int i = 0; i < nume; i++) {
-    VERTEX_INDEX iv0 = edge_vert[2*i];
-    VERTEX_INDEX iv1 = edge_vert[2*i+1];
+	for (int i = 0; i < nume; i++) {
+		VERTEX_INDEX iv0 = edge_vert[2*i];
+		VERTEX_INDEX iv1 = edge_vert[2*i+1];
 
-    cout << "[";
-    print_list(cout, coord+iv0*dim, dim);
-    cout << " ";
-    print_list(cout, coord+iv1*dim, dim);
-    cout << "]"<< endl;
-  }
+		cout << "[";
+		print_list(cout, coord+iv0*dim, dim);
+		cout << " ";
+		print_list(cout, coord+iv1*dim, dim);
+		cout << "]"<< endl;
+	}
 }
 
 
-void print_edge_info(int numv,vector <int> vert_degree)
+void print_edge_info(int numv, const COORD_TYPE * coord, vector <int> vert_degree)
 {
-  // header
-  cout <<"\n"<<setw(4)<<"vert"<<setw(5)<<"dg1"<<setw(5)
-  <<"dg3"<<setw(5)<<"dg>3"<<endl;
-   for (int i=0;i<numv;i++)
- { 
-  if (vert_degree[i]==1) 
-  cout <<setw(4)<<i<<setw(4)<<vert_degree[i]<<endl;
-  else if (vert_degree[i]==3)
-  cout <<setw(4)<<i<<setw(8)<<vert_degree[i]<<endl;
-  else if (vert_degree[i]>3)
-  cout <<setw(4)<<i<<setw(12)<<vert_degree[i]<<endl;
- }
+	// header
+	cout <<"\n"<<setw(4)<<"vert"<<setw(28)<<"position"<<setw(5)<<"dg1"<<setw(5)
+				  <<"dg3"<<setw(5)<<"dg>3"<<endl;
+	float crd[3]={0.0};
+
+	for (int i=0;i<numv;i++)
+	{
+		crd[0]= coord[3*i];crd[1]= coord[3*i+1];crd[2]= coord[3*i+2];
+
+		if (vert_degree[i]==1){
+			print_point(coord, vert_degree,  i, 1);
+			}
+		else if (vert_degree[i]==3){
+			print_point(coord, vert_degree,  i, 2);
+			}
+		else if (vert_degree[i]>3){
+			print_point(coord, vert_degree,  i, 3);
+					}
+	}
 }
 
 void output_vert_degree
 ( const int numv, vector <int> &vert_degree, const string &fname)
 {
-  vector <int> deg_one;
-  vector <int> deg_two;
-  vector <int> deg_threeandmore;  
-  for (int i=0;i<numv;i++)
-  {
-    if (vert_degree[i] == 0)
-      cnt0++;
-    else if (vert_degree[i] == 1){
-      //deg_one.push_back(i);
-      cnt1++;
-    }
-    else if (vert_degree[i] == 2){
-      //deg_two.push_back(i);
-      cnt2++;
-    }
-    else if (vert_degree[i] == 3){
-      //deg_threeandmore.push_back(i);
-      cnt3++;
-    }
-    else 
-    {
-      cntMoreThan3++;
-    }
-  }
+	vector <int> deg_one;
+	vector <int> deg_two;
+	vector <int> deg_threeandmore;
+	for (int i=0;i<numv;i++)
+	{
+		if (vert_degree[i] == 0)
+			cnt0++;
+		else if (vert_degree[i] == 1){
+			//deg_one.push_back(i);
+			cnt1++;
+		}
+		else if (vert_degree[i] == 2){
+			//deg_two.push_back(i);
+			cnt2++;
+		}
+		else if (vert_degree[i] == 3){
+			//deg_threeandmore.push_back(i);
+			cnt3++;
+		}
+		else
+		{
+			cntMoreThan3++;
+		}
+	}
 
-  cout <<"file name : "<<fname <<endl;
-  cout <<"Vertices with degree 0              ["<<cnt0<<"]" << endl;
-  cout <<"Vertices with degree 1              ["<<cnt1<<"]" << endl;
-  cout <<"Vertices with degree 2              ["<<cnt2<<"]" << endl;
-  cout <<"Vertices with degree 3              ["<<cnt3<<"]" << endl;
-  cout <<"Vertices with degree > 3            ["<<cntMoreThan3<<"]" << endl;
-  cout <<"Vertices with degree 1 or 3 or more ["<<cnt1 + cnt3 + cntMoreThan3<<"]" << endl;
-  cout <<"Total number of non 0 vertices      ["<<cnt1+cnt2+cnt3+cntMoreThan3<<"]" 
-    << endl;  
-  cout <<"Total number of vertices            ["<<numv<<"]" << endl;  
+	cout <<"file name : "<<fname <<endl;
+	cout <<"Vertices with degree 0              ["<<cnt0<<"]" << endl;
+	cout <<"Vertices with degree 1              ["<<cnt1<<"]" << endl;
+	cout <<"Vertices with degree 2              ["<<cnt2<<"]" << endl;
+	cout <<"Vertices with degree 3              ["<<cnt3<<"]" << endl;
+	cout <<"Vertices with degree > 3            ["<<cntMoreThan3<<"]" << endl;
+	cout <<"Vertices with degree 1 or 3 or more ["<<cnt1 + cnt3 + cntMoreThan3<<"]" << endl;
+	cout <<"Total number of non 0 vertices      ["<<cnt1+cnt2+cnt3+cntMoreThan3<<"]"
+			<< endl;
+	cout <<"Total number of vertices            ["<<numv<<"]" << endl;
 }
 
 void output_vert_degree_2_file 
 (const int numv, vector <int> &vert_degree)
 {
 
- vector <int> deg_one;
-  vector <int> deg_two;
-  vector <int> deg_threeandmore;  
-  for (int i=0;i<numv;i++)
-  {
-    if (vert_degree[i] == 0)
-      cnt0++;
-    else if (vert_degree[i] == 1){
-      //deg_one.push_back(i);
-      cnt1++;
-    }
-    else if (vert_degree[i] == 2){
-      //deg_two.push_back(i);
-      cnt2++;
-    }
-    else if (vert_degree[i] == 3){
-      //deg_threeandmore.push_back(i);
-      cnt3++;
-    }
-    else 
-    {
-      cntMoreThan3++;
-    }
-  }
+	vector <int> deg_one;
+	vector <int> deg_two;
+	vector <int> deg_threeandmore;
+	for (int i=0;i<numv;i++)
+	{
+		if (vert_degree[i] == 0)
+			cnt0++;
+		else if (vert_degree[i] == 1){
+			//deg_one.push_back(i);
+			cnt1++;
+		}
+		else if (vert_degree[i] == 2){
+			//deg_two.push_back(i);
+			cnt2++;
+		}
+		else if (vert_degree[i] == 3){
+			//deg_threeandmore.push_back(i);
+			cnt3++;
+		}
+		else
+		{
+			cntMoreThan3++;
+		}
+	}
 
-  cout << cnt0 <<" " <<  cnt1 << " "
-	   << cnt2 <<" " << cnt3 <<" "
-	   << cntMoreThan3 <<" "
-	   << cnt1 + cnt3 + cntMoreThan3<<" "
-	   << cnt1+cnt2+cnt3+cntMoreThan3<<" "
-	   << numv<<endl; 
+	cout << cnt0 <<" " <<  cnt1 << " "
+			<< cnt2 <<" " << cnt3 <<" "
+			<< cntMoreThan3 <<" "
+			<< cnt1 + cnt3 + cntMoreThan3<<" "
+			<< cnt1+cnt2+cnt3+cntMoreThan3<<" "
+			<< numv<<endl;
 }
 
 
@@ -133,38 +140,40 @@ void output_vert_degree_2_file
 (const int numv, vector <int> &vert_degree, const string &fname )
 {
 
- vector <int> deg_one;
-  vector <int> deg_two;
-  vector <int> deg_threeandmore;  
-  for (int i=0;i<numv;i++)
-  {
-    if (vert_degree[i] == 0)
-      cnt0++;
-    else if (vert_degree[i] == 1){
-      //deg_one.push_back(i);
-      cnt1++;
-    }
-    else if (vert_degree[i] == 2){
-      //deg_two.push_back(i);
-      cnt2++;
-    }
-    else if (vert_degree[i] == 3){
-      //deg_threeandmore.push_back(i);
-      cnt3++;
-    }
-    else 
-    {
-      cntMoreThan3++;
-    }
-  }
- /*
-  cout <<fname<<" "<< cnt0 <<" " <<  cnt1 << " "
-	   << cnt2 <<" " << cnt3 <<" "
-	   << cntMoreThan3 <<" "
-	   << cnt1 + cnt3 + cntMoreThan3<<" "
-	   << cnt1+cnt2+cnt3+cntMoreThan3<<" "
-	   << numv<<endl; 
-	   */
-	   cout <<fname<<" "<< cnt1 + cnt3 + cntMoreThan3;
+	vector <int> deg_one;
+	vector <int> deg_two;
+	vector <int> deg_threeandmore;
+	for (int i=0;i<numv;i++)
+	{
+		if (vert_degree[i] == 0)
+			cnt0++;
+		else if (vert_degree[i] == 1){
+			//deg_one.push_back(i);
+			cnt1++;
+		}
+		else if (vert_degree[i] == 2){
+			//deg_two.push_back(i);
+			cnt2++;
+		}
+		else if (vert_degree[i] == 3){
+			//deg_threeandmore.push_back(i);
+			cnt3++;
+		}
+		else
+		{
+			cntMoreThan3++;
+		}
+	}
+	cout <<fname<<" "<< cnt1 + cnt3 + cntMoreThan3;
+}
+/// HELPER FUNCTIONS
+void  print_point (const COORD_TYPE * coordList, vector <int> vert_degree, const int vertId, const int deg){
+	cout <<setw(5)<<vertId;
+	int vert = 3*vertId;
+	cout << fixed;
+	  cout << setprecision (5) ;
+	cout << " ["<<setw(6)<<coordList[vert]<<","<<setw(6)<<coordList[vert+1]<<","<<setw(6)<<coordList[vert+2]<<"]";
+	int w = deg*4;
+	cout <<setw(w)<< vert_degree[vertId]<<endl;
 }
 
--- src/findEdgeCount/findEdgeCountIO.h
+++ src/findEdgeCount/findEdgeCountIO.h
@@ -9,12 +9,13 @@
 void output_edges
 (const int dim, const COORD_TYPE * coord, const int numv,
  const VERTEX_INDEX * edge_vert, const int nume,vector<int> vert_degree);
-void print_edge_info(int num_vertices,vector <int> vert_degree);
+void print_edge_info(int num_vertices, const COORD_TYPE * coord, vector <int> vert_degree);
 void output_vert_degree
 ( const int numv, vector <int> &vert_degree, const string &fname);
 void output_vert_degree_2_file 
 (const int numv, vector <int> &vert_degree,const string &output_fn);
 void output_vert_degree_2_file 
 (const int numv, vector <int> &vert_degree);
+void  print_point (const COORD_TYPE * coordList, vector <int> vert_degree, const int vertId, const int deg);
 #endif
 
