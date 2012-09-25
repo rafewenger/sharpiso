1aa7b7e47d9a63feb333b494582c42eed4bd4f6a
Arindam Bhattacharya <arindamb86@gmail.com>
2012-09-25 12:58:35 -0400

    latest changes to singleVertCheck

--- src/py_scripts/singleVertCheck.py
+++ src/py_scripts/singleVertCheck.py
@@ -108,15 +108,27 @@ def setNrrdData(grid, height):
 					tempgv = grid[ind]
 					tempgv.sm = sm
 					if sm >=1 and sm <=7:
-						tempgv = grid[ind]
 						tempgv.isIntersect = True
           
   return grid
-        
+    
+'''
+compute the centroid of the object supplied as input 
+parameters
+input the vertex locations and the number of the vertices
+'''        
+def computeCentroid(vertLoc, numv):
+  sm = [0, 0, 0]
+  for i in range (numv):
+    sm[0] = sm[0] + vertLoc[3*i]
+    sm[1] = sm[1] + vertLoc[3*i+1]
+    sm[2] = sm[2] + vertLoc[3*i+2]    
+  centroid =[sm[x] / (numv) for x in range(3)]
+  return centroid
   
 def main():
-  if  len(sys.argv) != 8:
-    print ' parameters 1> .OFF file \n 2> height\n 3>scale 4> text file with nrrd data \n 5>origin'
+  if  len(sys.argv) != 9:
+    print ' parameters 1> .OFF file \n 2> height\n 3>scale 4> text file with nrrd data \n 5>origin 6 > longestDim'
     print sys.argv, 'length',len(sys.argv)
   else :
     data=[]
@@ -134,46 +146,60 @@ def main():
     
     #unit length
     scale = float(sys.argv[3])
-    
+    print '***********************'
     print 'scale', scale
-    origin = [int(sys.argv[5+x]) for x in range (3)]
-    print 'origin',origin
-    halfl = (1.0/scale)/2.0
-    ul = (1.0/scale)/(2**height)
-    print ' unit length ' , ul
+    originCompare = [int(sys.argv[5+x]) for x in range (3)]
+    print 'originCompare', originCompare
+    
+    L = float (sys.argv[8])
+    print 'longest dim ', L
+    print  'Dimesion (', L/scale ,')'
+    print 'new origin', [originCompare[x]-( (L*0.5/scale))  for x in range (3)]
+    NewOrigin = [originCompare[x]-( (L*0.5/scale))  for x in range (3)]
+    centroid = computeCentroid(vertLoc, numv)
+    halfl = (L/scale)/2.0
+    ul = (L /scale)/(2**height)
+    print 'unit length ' , ul
     numVertPerDirection = 2**height+1
-    print 'origin ',( origin[x] -halfl)
+    print "numVertPerDirection ", numVertPerDirection 
+    
+    print 'computed centroid ', centroid
+    print '***********************'
     for i in range (numv):
-      #baseLoc = [ int ( (vertLoc[3*i+x] - origin[x]+0.5) /ul ) for x in  range (3)]
-      baseLoc = [ int ( (vertLoc[3*i+x] - ( origin[x] - halfl)) /ul ) for x in  range (3)]
+      baseLoc = [ int ( (vertLoc[3*i+x] - ( centroid[x] - halfl)) /ul ) for x in  range (3)]
+      #debug
+      print baseLoc, 
       ind = baseLoc[0]*numVertPerDirection*numVertPerDirection + baseLoc[1]*numVertPerDirection+ baseLoc[2]
+      print ind, 
       tempgv = gridVertType()
       tempgv = grid[ind]
       tempgv.gridLoc=baseLoc
+      print tempgv.gridLoc
       tempgv.numIntersection = tempgv.numIntersection + 1 
       tempgv.ptsLocs.append([vertLoc[3*i+0] , vertLoc[3*i+1], vertLoc[3*i+2]])
     
     #read Nrrd data and set isIntersect
     NrrdsetGrid = setNrrdData(grid, height)
-    '''    
-    for i in  range (len(grid)):
-      tempgv = gridVertType()
-      tempgv = grid[i]
-      if tempgv.numIntersection > 1:
-        print  tempgv.x,',',tempgv.y,',',tempgv.z,' base loc  [',tempgv.gridLoc,'] -- numvert ', tempgv.numIntersection
-        print 'loc ',tempgv.ptsLocs
-        
-    '''    
-    '''
+    
+    print 'Should not have  a  point but does '  
     for i in range (numVertPerDirection):
-			for j in range (numVertPerDirection):
-				for k in range (numVertPerDirection):
-					n = (numVertPerDirection)*(numVertPerDirection)*i + (numVertPerDirection)*j + k
-					tempgv = gridVertType()
-					tempgv = grid[n]
-					if tempgv.isIntersect == True:
-						print  tempgv.x,tempgv.y,tempgv.z   
-    '''
+	  for j in range (numVertPerDirection):
+		  for k in range (numVertPerDirection):
+			  n = (numVertPerDirection)*(numVertPerDirection)*i + (numVertPerDirection)*j + k
+			  tempgv = gridVertType()
+			  tempgv = grid[n]
+			  if  tempgv.numIntersection > 0  and tempgv.isIntersect == False:
+			    print    'case 1',tempgv.x*ul ,tempgv.y*ul,tempgv.z*ul
+    
+    print 'Should have  a  point but does not '	     
+    for i in range (numVertPerDirection):
+	  for j in range (numVertPerDirection):
+		  for k in range (numVertPerDirection):
+			  n = (numVertPerDirection)*(numVertPerDirection)*i + (numVertPerDirection)*j + k
+			  tempgv = gridVertType()
+			  tempgv = grid[n]
+			  if  tempgv.numIntersection == 0  and tempgv.isIntersect == True:
+			    print    'case 2', tempgv.x*ul , tempgv.y*ul, tempgv.z*ul 
     
     print 'OUT'
     fw2 = open ('out1.off','w')
@@ -186,9 +212,10 @@ def main():
 					if  tempgv.numIntersection == 1 : 
 						#print  tempgv.gridLoc
 						#print   tempgv.gridLoc[0],' ',tempgv.gridLoc[1],' ',tempgv.gridLoc[2]
-						print >>fw2, " ".join(map(str, tempgv.ptsLocs[0])), ' grid location ',
-						print >>fw2, tempgv.gridLoc
-
+						#print >>fw2, " ".join(map(str, tempgv.ptsLocs[0])), ' grid location ',
+						#print >>fw2, tempgv.gridLoc
+						print >> fw2 , ' '.join(map(str,[tempgv.ptsLocs[0][x] - ( centroid[x] - halfl) for x in range (3)]))
+    
     print 'OUT2'
     fw3 = open ('out2.off','w')
     for i in range (numVertPerDirection):
@@ -197,15 +224,31 @@ def main():
 					n = (numVertPerDirection)*(numVertPerDirection)*i + (numVertPerDirection)*j + k
 					tempgv = gridVertType()
 					tempgv = grid[n]
-					if  tempgv.numIntersection > 1:
-					  print >>fw3, 'grid loc ',[tempgv.gridLoc[x] *ul for x in range (3)]
+					if  tempgv.numIntersection > 0:
+					  print >>fw3,'NUM POINTS ', tempgv.numIntersection
+					  print >>fw3, 'grid loc ',n,' -- ',[tempgv.gridLoc[x]  for x in range (3)],'--',[tempgv.gridLoc[x]*ul  for x in range (3)],' \n '
 					  for m in  range (len(tempgv.ptsLocs)): 
 						  #print >>fw3," ".join(map(str, tempgv.ptsLocs[m])),' grid location ',
 						   
-						  print >> fw3 , ' '.join(map(str,[tempgv.ptsLocs[m][x] - ( origin[x] - halfl) for x in range (3)]))
-						  print >> fw3 ,'diff', [(tempgv.ptsLocs[m][x] - ( origin[x] - halfl))-tempgv.gridLoc[x] *ul  for x in range (3)],'--', ul
+						  print >> fw3 , ' '.join(map(str,[tempgv.ptsLocs[m][x] - ( centroid[x] - halfl) for x in range (3)])),
+						  print >> fw3 , '[',' '.join(map(str,[tempgv.ptsLocs[m][x] for x in range (3)])),']',
+						  print >> fw3 ,'diff', [(tempgv.ptsLocs[m][x] - ( centroid[x] - halfl))-tempgv.gridLoc[x] *ul  for x in range (3)]
+						  diff = [(tempgv.ptsLocs[m][x] - ( centroid[x] - halfl))-tempgv.gridLoc[x] *ul  for x in range (3)]
+						  if abs(diff[0] - 0.0)<0.001 or  abs(diff[1] - 0.0)<0.001 or abs(diff[2] - 0.0)<0.001:
+						    print >>fw3 , 'FALSE POSITIVE'
 					  print >>fw3, ' '
-						  
+
+    print 'OUT3'#####  all points with more than 1 points
+    fw4 = open ('outMore.off','w')
+    for i in range (numVertPerDirection):
+			for j in range (numVertPerDirection):
+				for k in range (numVertPerDirection):
+					n = (numVertPerDirection)*(numVertPerDirection)*i + (numVertPerDirection)*j + k
+					tempgv = gridVertType()
+					tempgv = grid[n]
+					if  tempgv.numIntersection > 1 :
+					  for m in  range (len(tempgv.ptsLocs)): 
+						  print >> fw4 , ' '.join(map(str,[tempgv.ptsLocs[m][x]- ( centroid[x] - halfl) for x in range (3)]))						  
 						  #print >> fw3, tempgv.gridLoc
 if __name__ == "__main__":
     main()
