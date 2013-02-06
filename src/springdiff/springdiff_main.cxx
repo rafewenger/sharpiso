32c5c60d53ae3f8234dce6fdce3e581b1c23669d
Arindam Bhattacharya <arindamb86@gmail.com>
2013-02-06 14:22:45 -0500

    updating springdiff

--- src/springdiff/springdiff_main.cxx
+++ src/springdiff/springdiff_main.cxx
@@ -19,7 +19,7 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 
 
 #include <iostream>
@@ -28,10 +28,11 @@
 #include "ijkgrid_nrrd.txx"
 
 #include "springdiff.h"
-//#include "isodual3D_datastruct.h"
+#include "springdiff_info.h"
 #include "sharpiso_grids.h"
+
+
 using namespace IJK;
-//using namespace ISODUAL3D;
 using namespace SHARPISO;
 
 // global variables
@@ -50,7 +51,7 @@ using namespace std;
 // local subroutines
 void memory_exhaustion();
 void usage_error();
-void parse_command_line(int argc, char **argv);
+void parse_command_line(int argc, char **argv, INFO &inf);
 
 // **************************************************
 // MAIN
@@ -58,62 +59,64 @@ void parse_command_line(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
-  time_t start_time;
-  time(&start_time);
+	time_t start_time;
+	time(&start_time);
 
-  IJK::ERROR error;
+	IJK::ERROR error;
 
-  try {
+	try {
 
-    std::set_new_handler(memory_exhaustion);
+		std::set_new_handler(memory_exhaustion);
+		INFO inf;
 
-    parse_command_line(argc, argv);
+		parse_command_line(argc, argv, inf);
+		inf.in_info.print_input_info();
+		SHARPISO_SCALAR_GRID full_scalar_grid;
+		GRID_NRRD_IN<int,int> nrrd_in;
+		NRRD_DATA<int,int> nrrd_header;
 
-    SHARPISO_SCALAR_GRID full_scalar_grid;
-    GRID_NRRD_IN<int,int> nrrd_in;
-    NRRD_DATA<int,int> nrrd_header;
+		nrrd_in.ReadScalarGrid
+		(scalar_filename, full_scalar_grid, nrrd_header, error);
+		if (nrrd_in.ReadFailed()) { throw error; }
 
-    nrrd_in.ReadScalarGrid
-      (scalar_filename, full_scalar_grid, nrrd_header, error);
-    if (nrrd_in.ReadFailed()) { throw error; }
+		GRADIENT_GRID gradient_grid;
+		// compute the central difference
+		compute_gradient_central_difference(full_scalar_grid, gradient_grid);
+		// compute the spring diffusion
+		compute_spring_diffusion(full_scalar_grid, num_iter, lambda, mu, gradient_grid);
 
-    GRADIENT_GRID gradient_grid;
-    compute_gradient_central_difference(full_scalar_grid, gradient_grid);
-      
-    compute_spring_diffusion(full_scalar_grid, num_iter, lambda, mu, gradient_grid);
-      
 
-    if (flag_gzip) {
-      write_vector_grid_nrrd_gzip(gradient_filename, gradient_grid);
-    }
-    else {
-      write_vector_grid_nrrd(gradient_filename, gradient_grid);
-    }
+		if (flag_gzip) {
+			write_vector_grid_nrrd_gzip(gradient_filename, gradient_grid);
+		}
+		else {
+			write_vector_grid_nrrd(gradient_filename, gradient_grid);
+		}
 
-    if (report_time_flag) {
+		if (report_time_flag) {
 
-      time_t end_time;
-      time(&end_time);
-      double total_elapsed_time = difftime(end_time, start_time);
+			time_t end_time;
+			time(&end_time);
+			double total_elapsed_time = difftime(end_time, start_time);
 
-      cout << endl;
-      cout << "Elapsed time = " << total_elapsed_time << " seconds." 
-           << endl;
-    };
+			cout << endl;
+			cout << "Elapsed time = " << total_elapsed_time << " seconds."
+					<< endl;
+		};
 
-  } 
-  catch (ERROR error) {
-    if (error.NumMessages() == 0) {
-      cerr << "Unknown error." << endl;
-    }
-    else { error.Print(cerr); }
-    cerr << "Exiting." << endl;
-    exit(20);
-  }
-  catch (...) {
-    cerr << "Unknown error." << endl;
-    exit(50);
-  };
+	}
+	catch (ERROR error) {
+		if (error.NumMessages() == 0) {
+			cerr << "Unknown error." << endl;
+		}
+		else { error.Print(cerr); }
+		cerr << "Exiting." << endl;
+		exit(20);
+	}
+	catch (...) {
+		cerr << "Unknown error." << endl;
+		exit(50);
+	};
 
 }
 
@@ -124,57 +127,62 @@ int main(int argc, char **argv)
 
 void memory_exhaustion()
 {
-  cerr << "Error: Out of memory.  Terminating program." << endl;
-  exit(10);
+	cerr << "Error: Out of memory.  Terminating program." << endl;
+	exit(10);
 }
 
-void parse_command_line(int argc, char **argv)
+void parse_command_line(int argc, char **argv, INFO &inf)
 {
-  int iarg = 1;
+	int iarg = 1;
 
-  while (iarg < argc && argv[iarg][0] == '-') {
-    if (string(argv[iarg]) == "-time") 
-      { report_time_flag = true;   }
-    else if (string(argv[iarg]) == "-gzip")
-      { flag_gzip = true; }
-    else if (string(argv[iarg]) == "-num_iter")
-    {iarg++;
-        if (iarg >= argc) { usage_error(); };
-        sscanf(argv[iarg], "%d", &num_iter);
-    }
-    else if (string(argv[iarg]) == "-lambda")
-    {iarg++;
-        if (iarg >= argc) { usage_error(); };
-        sscanf(argv[iarg], "%f", &lambda);
-    }
-    else if (string(argv[iarg]) == "-mu")
-    {iarg++;
-        if (iarg >= argc) { usage_error(); };
-        sscanf(argv[iarg], "%f", &mu);
-    }
-    else 
-      { usage_error(); }
-    iarg++;
-  }
+	while (iarg < argc && argv[iarg][0] == '-') {
+		if (string(argv[iarg]) == "-time")
+		{ report_time_flag = true;   }
+		else if (string(argv[iarg]) == "-gzip")
+		{ flag_gzip = true; }
+		else if (string(argv[iarg]) == "-num_iter")
+		{
+			iarg++;
+			if (iarg >= argc) { usage_error(); };
+			sscanf(argv[iarg], "%d", &num_iter);
+			inf.in_info.num_iteration=atoi(argv[iarg]);
+		}
+		else if (string(argv[iarg]) == "-lambda")
+		{
+			iarg++;
+			if (iarg >= argc) { usage_error(); };
+			sscanf(argv[iarg], "%f", &lambda);
+			inf.in_info.lambda=atof(argv[iarg]);
+		}
+		else if (string(argv[iarg]) == "-mu")
+		{
+			iarg++;
+			if (iarg >= argc) { usage_error(); };
+			sscanf(argv[iarg], "%f", &mu);
+			inf.in_info.mu=atof(argv[iarg]);
+		}
+		else
+		{ usage_error(); }
+		iarg++;
+	}
 
-  if (iarg+2 != argc) { usage_error(); };
+	if (iarg+2 != argc) { usage_error(); };
 
-  scalar_filename = argv[iarg];
-  gradient_filename = argv[iarg+1];
+	scalar_filename = argv[iarg];
+	gradient_filename = argv[iarg+1];
 }
 
 void usage_msg()
 {
-    cerr << "Usage: springdiff [-gzip] [-time] \n"
-    <<" -num_iter <n>  number of iterations.\n"
-    <<" -lambda <f> \n"
-    <<" -mu <f>\n"
-    <<"{scalar nrrd file} {gradient nrrd file}"
-    << endl;
+	cerr << "Usage: springdiff [-gzip] [-time] [OPTIONS] {scalar nrrd file} {gradient nrrd file}"<<endl;
+	cerr <<"options: "<<endl;
+	cerr <<"\t\t-num_iter <n>  number of iterations."<<endl;
+	cerr <<"\t\t-lambda <f>"<<endl;
+	cerr<<"\t\t-mu <f>\n" << endl;
 }
 
 void usage_error()
 {
-  usage_msg();
-  exit(100);
+	usage_msg();
+	exit(100);
 }
